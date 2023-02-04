de00f8cb5480259f696ce64e3fb78d5709b2858d
Delfor <delfor_10_11@hotmail.es>
2023-02-04 11:20:48 -0300

    Agregando React-Router

--- portal_trabajo/src/App.js
+++ portal_trabajo/src/App.js
@@ -6,16 +6,19 @@ import SelectMode from './Components/SelectMode';
 import Profiles from './Components/Profiles';
 import SaveProfile from './Components/SaveProfile';
 import Login from './Components/Login';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
    <>
+   <BrowserRouter>
    <NavBar/>
- {/*  <SelectMode/>  */}
-  {/* <Profiles/>   */}
- {/* <SaveProfile/> */}
-   <Login/>
+   <Routes>
+    <Route path="/profiles" element={<Profiles/>} />
+   </Routes>
    <Footer/>
+   </BrowserRouter>
+   
    </>
   );
 }
--- portal_trabajo/src/Components/NavBar.js
+++ portal_trabajo/src/Components/NavBar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from "../assets/img/logo.png";
 import userImg from "../assets/img/usuario.png";
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
   return (
@@ -12,8 +13,8 @@ const NavBar = () => {
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
             </label>
             <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                <li><a>Homepage</a></li>
-                <li><a>Portfolio</a></li>
+                <li><Link to='/'>Inicio</Link></li>
+                <li><Link to='/profiles'>Perfiles</Link></li>
                 <li><a>About</a></li>
             </ul>
             </div>
