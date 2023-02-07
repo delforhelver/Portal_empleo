88674088985352e016efd0ef506141f38e7718bf
Delfor <delfor_10_11@hotmail.es>
2023-02-07 20:23:17 -0300

    Creando login

--- portal/src/main/java/com/proyecto/portal/controllers/AuthController.java
+++ portal/src/main/java/com/proyecto/portal/controllers/AuthController.java
@@ -1,6 +1,11 @@
 package com.proyecto.portal.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,18 +28,19 @@ public class AuthController {
     @CrossOrigin(origins = "http://localhost:3000")
     @RequestMapping(value = "/login")
     @PostMapping
-    public String login(@RequestBody UsuarioModel usuario) {
+    public ResponseEntity<Map<String, Object>> login(@RequestBody UsuarioModel usuario) {
       UsuarioModel usuarioLogueado = usuarioService.obtenerUsuarioPorCredenciales(usuario);
-      
+  
       if (usuarioLogueado != null) {
-
-         String token = jwtUtil.create(String.valueOf(usuarioLogueado.getUser_id()), usuarioLogueado.getEmail());
-         return token; }
-      else{
-        return "FAIL";
+        String token = jwtUtil.create(String.valueOf(usuarioLogueado.getUser_id()), usuarioLogueado.getEmail());
+        usuarioLogueado.setContrasena("");
+        Map<String, Object> response = new HashMap<>();
+        response.put("usuario", usuarioLogueado);
+        response.put("token", token);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+      } else {
+        return new ResponseEntity<>(null, HttpStatus.UNAUTHORIZED);
       }
-
-
     }
 
 }
--- portal_trabajo/src/Components/Login.js
+++ portal_trabajo/src/Components/Login.js
@@ -1,38 +1,114 @@
-import React from 'react'
+import React, {useState} from "react";
+import axios from "axios";
+import "./css/Login.css";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const [datos, setDatos] = useState({
+    usuario: "",
+    contrasena: "",
+    email: "",
+    tipo_usuario: "",
+  });
+
+  const handleInputChange = (e) => {
+    let {name, value} = e.target;
+    let newDatos = {...datos, [name]: value};
+    setDatos(newDatos);
+  };
+
+  const navigate = useNavigate();
+  const [error, setError] = useState(null);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!e.target.checkValidity()) {
+      console.log("No pasa");
+    } else {
+      try {
+        let res = await axios.post("http://localhost:8080/login", datos);
+        console.log(res.data);
+        navigate('/');
+      } catch (error) {
+        setError("Usuario o contraseña incorrectos. Intente nuevamente por favor.");
+      }
+    }
+  };
+
   return (
     <div className="hero min-h-screen bg-base-200">
-  <div className="hero-content flex-col lg:flex-row-reverse">
-    <div className="text-center lg:text-left">
-      <h1 className="text-5xl font-bold">Login now!</h1>
-      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-    </div>
-    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-      <div className="card-body">
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Email</span>
-          </label>
-          <input type="text" placeholder="email" className="input input-bordered" />
+     {error && <div className="alert alert-error shadow-lg mensaje_error">
+        <div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="stroke-current flex-shrink-0 h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+          <span>{error}</span>
         </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Password</span>
-          </label>
-          <input type="text" placeholder="password" className="input input-bordered" />
-          <label className="label">
-            <a href="#" className="label-text-alt link link-hover">¿Ha olvidado la contraseña?</a>
-          </label>
-        </div>
-        <div className="form-control mt-6">
-          <button className="btn btn-primary">Login</button>
-        </div>
+      </div> }
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <form onSubmit={handleSubmit}>
+          <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold">Login now!</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+          </div>
+          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <div className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="email"
+                  name="email"
+                  className="input input-bordered"
+                  onChange={handleInputChange}
+                  value={datos.email}
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="password"
+                  name="contrasena"
+                  className="input input-bordered"
+                  onChange={handleInputChange}
+                  value={datos.contrasena}
+                />
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">
+                    ¿Ha olvidado la contraseña?
+                  </a>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <button type="submit" className="btn btn-primary">
+                  Login
+                </button>
+              </div>
+            </div>
+          </div>
+        </form>
       </div>
     </div>
-  </div>
-</div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
